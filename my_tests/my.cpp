cc14332c0cd11d16094b96bde4fb25b26d0a428a
Daniel Perez <danielperezdeandres@gmail.com>
2016-09-08 18:12:49 +0200

    All current tests pass

--- my_tests/my.cpp
+++ my_tests/my.cpp
@@ -1,7 +1,26 @@
 #include "my.hpp"
 
+#include <iostream>
+#include <sstream> 
+
+/* Got the idea for this from here:
+ * http://stackoverflow.com/questions/1894886/parsing-a-comma-delimited-stdstring*/
 int Add(std::string numbers)
 {
-	return 0;
+	std::string delimiter {","};
+	int sum {0};
 
+	std::stringstream ss(numbers);
+
+	int i;
+
+	while(ss >> i)
+	{
+		sum+=i;
+
+		if(ss.peek() == ',')
+			ss.ignore();
+	}
+
+	return sum;
 }
