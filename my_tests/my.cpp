50c9ff069060c193a91701ee6468db644da40689
Daniel Perez <danielperezdeandres@gmail.com>
2016-09-20 17:00:27 +0200

    TDD example finished, with all tests passing #1 completed

--- my_tests/my.cpp
+++ my_tests/my.cpp
@@ -1,7 +1,20 @@
 #include "my.hpp"
 
 #include <iostream>
-#include <sstream> 
+#include <sstream>
+#include <stdexcept>
+
+constexpr size_t single_delim = 3;
+constexpr int big_number_limit = 1001;
+
+void prep_input(std::string& numbers, const std::string& delimiter)
+{
+	for(size_t x = 0; x != std::string::npos; x = numbers.find(delimiter))
+	{
+		numbers.replace(x, delimiter.length(), ",");
+	}
+	return;
+}
 
 /* Got the idea for this from here:
  * http://stackoverflow.com/questions/1894886/parsing-a-comma-delimited-stdstring*/
@@ -10,36 +23,63 @@ int Add(std::string numbers)
 	std::string delimiter {","};
 	int sum {0};
 
-	size_t p = 0;
+	size_t p {0};
+	size_t q {0};
 
 	int i;
 	if (numbers.length()>0) {
 		if(numbers.find("//")==0) {
 			p = numbers.find('\n');
 			if (p == std::string::npos) {
-				printf("Wrong delimiter formatting. It should be: //[delimiter]\\n[numbers…]\n");
-				return 0;
+				throw std::logic_error("Wrong delimiter formatting. It should be: //[delimiter]\\n[numbers…]");
+
 			}
 
-			delimiter=numbers.substr(2,p-2);
-			for (size_t x; x != std::string::npos; x = numbers.find(delimiter))
-			      {
-			      numbers.replace(x,delimiter.length(),",");
+			if (p > single_delim) {
+				if (numbers.find("//[") == 0){
+					p = numbers.find("[");
+					while ( p != std::string::npos) {
+						q = numbers.find("]");
+						if (q !=  std::string::npos) {
+							delimiter = numbers.substr(p+1, q-p-1);
 
-			      }
+
+							numbers.erase(numbers.begin()+q);
+							numbers.erase(numbers.begin()+p);
+
+							prep_input(numbers, delimiter);
+
+						}
+						else {
+							throw std::logic_error(" Wrong formatting");
+						}
+						p = numbers.find("[");
+					}
+				}else {
+					throw std::logic_error(" Wrong formatting");
+				}
+			}
+			else{
+				delimiter = numbers.substr(2,p-2);
+			}
+
+			prep_input(numbers, delimiter);
 			p = numbers.find('\n');
 
 		}
 	}
 
+
 	std::stringstream ss(numbers.substr(p));
 	while(ss >> i)
 	{
+
 		sum+=i;
+		if (i < 0) throw std::invalid_argument("Negatives not allowed " + i);
 
 		if(ss.peek() == ',' || ss.peek() == '\n')
 			ss.ignore();
 	}
 
-	return sum;
+	return sum%big_number_limit;
 }
--- my_tests/my_tests.cpp
+++ my_tests/my_tests.cpp
@@ -13,7 +13,6 @@ TEST(AddTest,HandlesSingleNumberInput)
 {
 	EXPECT_EQ(1,Add("1"));
 	EXPECT_EQ(2,Add("2"));
-
 	EXPECT_EQ(10,Add("10"));
 
 }
@@ -26,6 +25,16 @@ TEST(AddTest,HandleTwoNumberInput)
 
 }
 
+TEST(AddTest,Handle_big_numbers)
+{
+	EXPECT_EQ(500,Add("100,250,140,10"));
+	EXPECT_EQ(1000,Add("100,500,300,100"));
+	EXPECT_EQ(1000,Add("1000"));
+	EXPECT_EQ(0,Add("1001"));
+	EXPECT_EQ(2,Add("1001,2"));
+	EXPECT_EQ(500,Add("501,300,300,200,100,100"));
+}
+
 TEST(AddTest, HandleManyNumberInput)
 {
 	EXPECT_EQ(3,Add("1,1,1"));
@@ -33,6 +42,10 @@ TEST(AddTest, HandleManyNumberInput)
 	EXPECT_EQ(120,Add("10,10,10,10,20,20,5,5,5,5,5,5,5,5"));
 }
 
+TEST(AddTest, negative_numbers)
+{
+	ASSERT_ANY_THROW(Add("1,-1"));
+}
 TEST(AddTest, HandleNewLine)
 {
 	EXPECT_EQ(2,Add("1\n1"));
@@ -43,9 +56,15 @@ TEST(AddTest, HandleNewLine)
 
 TEST(AddTest, HandleNewDelimiter)
 {
-	EXPECT_EQ(2,Add("//;\n1;1"));
-	EXPECT_EQ(0,Add("//;1;1"));
-	EXPECT_EQ(15,Add("//-|-\n1-|-1-|-3-|-5-|-5"));
+	ASSERT_EQ(2,Add("//;\n1;1"));
+//	ASSERT_EQ(0,Add("//;1;1"));
+	ASSERT_ANY_THROW(Add("//;1;1"));
+
+//	ASSERT_EQ(0,Add("//-|-\n1-|-1-|-3-|-5-|-5"));
+	ASSERT_ANY_THROW(Add("//-|-\n1-|-1-|-3-|-5-|-5"));
+
+	ASSERT_EQ(15,Add("//[-|-]\n1-|-1-|-3-|-5-|-5"));
+	EXPECT_EQ(20,Add("//[..][$¡]\n1..1$¡3$¡3$¡2..4..4$¡1..1"));
 }
 
 
